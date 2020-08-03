fc6cda9e7fefe05f686d340760e736b6a5e583ca
ashleycrouch <ashley.crouch@rocketmail.com>
2020-08-03 16:39:06 -0500

    hopefully will be last change for reals

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -4,5 +4,7 @@
 mkdir secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output secrets/firebase-adminsdk.json firebase-adminsdk.json.gpg
+# gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+# --output secrets/firebase-adminsdk.json D:/Projects/Tracker/firebase-adminsdk.json.gpg
+
+gpg --quiet --batch --yes --output secrets/firebase-adminsdk.json --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" firebase-adminsdk.json.gpg
