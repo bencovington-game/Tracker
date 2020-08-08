5ada1178d92bddbeee793699c34ee5287c3fecf9
ashleycrouch <ashley.crouch@rocketmail.com>
2020-08-08 10:14:02 -0500

    Created a Login page that doesn't work

--- Tracker/Tracker/Controllers/HomeController.cs
+++ Tracker/Tracker/Controllers/HomeController.cs
@@ -11,7 +11,12 @@ using Microsoft.AspNetCore.Authorization;
 using Google.Api;
 using FirebaseAdmin;
 using FirebaseAdmin.Auth;
+using AspNetCore.Firebase.Authentication;
 using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Identity;
+using Google.Apis.Util;
+using System.Linq.Expressions;
+using System.Collections.ObjectModel;
 
 namespace Tracker.Controllers
 {
@@ -30,26 +35,14 @@ namespace Tracker.Controllers
             return "This endpoint is protected";
         }
 
-        //public async Task<IActionResult> SignInAsync()
-        //{
-        //    FirebaseApp app = FirebaseApp.Create();
-        //    FirebaseAuth auth = FirebaseAuth.GetAuth(app);
-        //    UserRecordArgs args = new UserRecordArgs()
-        //    {
-        //        Uid = "Test-Uid",
-        //        Email = "user@example.com",
-        //        EmailVerified = false
-        //    };
-        //    UserRecord userRecord = await FirebaseAuth.DefaultInstance.CreateUserAsync(args);
-        //    return Ok("Signed in");
-        //}
-
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> SignUpAsync(LoginViewModel signinModel)
         {
             FirebaseApp app = FirebaseApp.Create();
             FirebaseAuth auth = FirebaseAuth.GetAuth(app);
+
+            
             if (ModelState.IsValid)
             {
                 UserRecordArgs args = new UserRecordArgs()
@@ -69,52 +62,74 @@ namespace Tracker.Controllers
         }
 
 
-
         [HttpGet]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LogInAsync(LoginViewModel loginModel)
+        {
+
+            //FirebaseApp app = FirebaseApp.Create();
+            //FirebaseAuth auth = FirebaseAuth.GetAuth(app);
+            //IReadOnlyDictionary<string, object> claims 
+            //    = new Dictionary<string, object> 
+            //{ 
+            //        { "Email", loginModel.Email }, 
+            //        { "Password", loginModel.Password }
+            //};
+            //UserRecord rec = await auth.GetUserByEmailAsync(loginModel.Email);
+            //await auth.SetCustomUserClaimsAsync(rec.Uid, claims);
+
+            //string token = await auth.CreateCustomTokenAsync(userRec.Uid);
+
+            //if (ModelState.IsValid)
+            //{
+
+            //}
+
+
+
+            //FirebaseApp app = FirebaseApp.Create();
+            //FirebaseAuth auth = FirebaseAuth.GetAuth(app);
+
+            //if(ModelState.IsValid)
+            //{
+
+
+
+            //    UserRecordArgs args = new UserRecordArgs()
+            //    {   Email = loginModel.Email,
+            //        Password = loginModel.Password
+            //    };
+            //    //FirebaseAuth.DefaultInstance.
+            //    //UserRecord userRecord = await FirebaseAuth.DefaultInstance.
+            //    //FirebaseAuth.DefaultInstance.CreateCustomTokenAsync(User.)
+            //    UserRecord fbUser = await FirebaseAuth.DefaultInstance.GetUserByEmailAsync(args.Email);
+            //    try
+            //    {
+            //        var token = await FirebaseAuth.DefaultInstance.CreateCustomTokenAsync(fbUser.Uid);
+            //    }
+            //    catch(Exception e)
+            //    {
+            //        return this.StatusCode(401, new { Error = e.Message });
+            //    }
+            //    //Firebase token = await FirebaseAuth.DefaultInstance.CreateCustomTokenAsync(fbUser.Uid);
+            //}
+            return Ok("Logged in");
+        }
+
         public IActionResult Login()
         {
             return View();
         }
 
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> UserLogin([Bind] User user)
-        //{
-        //    if(ModelState.IsValid)
-        //    {
-                
-        //    }
-        //}
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-        //[HttpPost]
-        //public async Task<IActionResult> SignInAsync()
-        //{
-        //    //UserRecordArgs args = new UserRecordArgs()
-        //    //{
-        //    //    Uid = "test-uid",
-        //    //    Email = "user@example.com",
-        //    //};
-        //    //UserRecord record = await FirebaseAuth.DefaultInstance.CreateUserAsync(args);
-        //    return Ok("logged in");
-        //}
-
-
-        //private readonly ILogger<HomeController> _logger;
-
-        //public HomeController(ILogger<HomeController> logger)
-        //{
-        //    _logger = logger;
-        //}
-
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
-
-        //public IActionResult Privacy()
-        //{
-        //    return View();
-        //}
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
         //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         //public IActionResult Error()
